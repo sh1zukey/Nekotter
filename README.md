7b60d7571ae56d0c00cd0c456db0ce6e9fb7e95c
takashun <takashun.0704@gmail.com>
2020-07-04 23:47:06 +0900

    README

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Nekotter
-
 > Neko is GOD.
+
+自分がリツイートした猫関連のツイートを自動収集して日付別で見れるWEBアプリケーションです。
+
+[最新の検索結果](https://app.takashun.dev/date/latest)
+
+## 技術
+nuxt + expressでフロントとバックどっちも書いてます。
+/api/v1/api.jsがAPI部分
+
+収集はAM2時にcronでpythonスクリプトを動かしています。
+[スクリプト](https://gist.github.com/takashun/c03733da6438a6f94f154b86828b0ba4)
+
+## 今後
+- Dockerで動かせると便利かな
+- 今は完全に自分用なのでちゃんとしたWEBサービスとして公開してみたい
 
 ## Build Setup
 
--- components/ImgTag.vue
+++ components/ImgTag.vue
@@ -34,34 +34,11 @@ export default {
   },
   methods: {
     replace(url) {
-      // this.imageUrl = 'https://placehold.jp/3d4070/ffffff/800x500.png?text=404'
+      this.imageUrl = 'https://placehold.jp/3d4070/ffffff/800x500.png?text=404'
 
-      const name = 'format'.replace(/[[\]]/g, '\\$&')
-      const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
-      const results = regex.exec(url)
-      if (!results) return null
-      if (!results[2]) return ''
-      const format = decodeURIComponent(results[2].replace(/\+/g, ' '))
-
-      const tempUrl = url.replace(/\?.*$/, '').split('/')
-
-      const date = new Date(this.date)
-
-      const y = date.getFullYear()
-      const m = ('00' + (date.getMonth() + 1)).slice(-2)
-      const d = ('00' + date.getDate()).slice(-2)
-      const textDate = y + '-' + m + '-' + d
-
-      const newUrl =
-        'https://apa.ittekikun.com/twviewer/images/' +
-        textDate +
-        '/' +
-        tempUrl[4] +
-        '.' +
-        format
-      this.dataImageUrl = newUrl
-      this.$parent.tweet.for_lbox_media_urls[this.id].thumb = newUrl
-      this.$parent.tweet.for_lbox_media_urls[this.id].src = newUrl
+      this.dataImageUrl = this.imageUrl
+      this.$parent.tweet.for_lbox_media_urls[this.id].thumb = this.imageUrl
+      this.$parent.tweet.for_lbox_media_urls[this.id].src = this.imageUrl
     }
   }
 }
