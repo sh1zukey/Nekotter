4f313db190f30df108e0e47ab8a9465eb735b2ef
takashun <s1832087py@s.chibakoudai.jp>
2020-12-17 22:19:44 +0900

    LightBoxをコンポーネント化

--- components/ImageCard.vue
+++ components/ImageCard.vue
@@ -1,30 +1,34 @@
 <template>
   <div class="h-100">
-    <CoolLightBox
-      :items="tweet.for_lbox_media_urls"
-      :index="index"
-      :loop="false"
-      :close-on-click-outside-mobile="true"
-      @close="index = null"
-    >
-    </CoolLightBox>
     <div class="card">
       <div class="detection">
-        <div class="card-links">
-          <b-button-group vertical>
-            <b-button
-              variant="primary"
-              :href="'https://twitter.com/i/status/' + tweet.status_id"
-              target="_blank"
-              ><font-awesome-icon :icon="['fab', 'twitter']"
-            /></b-button>
-            <b-button variant="secondary" :to="'../userid/' + tweet.user_id"
-              ><font-awesome-icon :icon="['fas', 'user']"
-            /></b-button>
-            <b-button variant="info" @click="index = 0"
-              ><font-awesome-icon :icon="['fas', 'expand']"
-            /></b-button>
-          </b-button-group>
+        <div v-if="tweet.video_flag === 1" class="card-info">
+          <b-badge><font-awesome-icon :icon="['fas', 'video']" /></b-badge>
+        </div>
+        <div class="card-links d-flex flex-column">
+          <div>
+            <b-button-group vertical>
+              <b-button
+                variant="primary"
+                :href="'https://twitter.com/i/status/' + tweet.status_id"
+                target="_blank"
+                ><font-awesome-icon :icon="['fab', 'twitter']"
+              /></b-button>
+              <b-button variant="secondary" :to="'../userid/' + tweet.user_id"
+                ><font-awesome-icon :icon="['fas', 'user']"
+              /></b-button>
+              <b-button variant="info" @click="lightbox_event(0)"
+                ><font-awesome-icon :icon="['fas', 'expand']"
+              /></b-button>
+            </b-button-group>
+          </div>
+          <div v-if="tweet.thread_flag === 1" class="mt-2">
+            <b-button-group vertical>
+              <b-button variant="success" :to="'../thread/' + tweet.status_id"
+                ><font-awesome-icon :icon="['fas', 'list-ul']"
+              /></b-button>
+            </b-button-group>
+          </div>
         </div>
         <div class="card-author text-light">
           <span>{{ tweet.user_name }}</span>
@@ -145,13 +149,9 @@
   </div>
 </template>
 <script>
-import CoolLightBox from 'vue-cool-lightbox'
-import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
 import ImgTag from '~/components/ImgTag.vue'
-
 export default {
   components: {
-    CoolLightBox,
     ImgTag,
   },
   props: {
@@ -162,10 +162,18 @@ export default {
   },
   data() {
     return {
-      index: null,
+      lightBoxData: {
+        mediaUrls: [],
+        index: null,
+      },
     }
   },
   methods: {
+    lightbox_event(index) {
+      this.lightBoxData.index = 0
+      this.lightBoxData.mediaUrls = this.tweet.for_lbox_media_urls
+      this.$nuxt.$emit('LIGHT_BOX_EVENT', this.lightBoxData)
+    },
     nl2br(str) {
       str = str.replace(/\r\n/g, '<br />')
       str = str.replace(/(\n|\r)/g, '<br />')
@@ -180,7 +188,6 @@ export default {
       const regexpMakeLink = function (all, url, h, href) {
         return '<a href="h' + href + '" target="_blank">' + url + '</a>'
       }
-
       return str.replace(regexpUrl, regexpMakeLink)
     },
   },
@@ -196,7 +203,6 @@ export default {
   border-radius: calc(0.25rem - 1px);
   overflow: hidden;
 }
-
 .detection {
   position: absolute;
   top: 0;
@@ -204,17 +210,27 @@ export default {
   bottom: 0;
   left: 0;
   content: ' ';
-
   margin-right: auto;
   margin-top: auto;
   margin-bottom: auto;
-
   height: 90%;
   width: 70%;
-
   z-index: 2;
 }
-
+.card-info {
+  position: absolute;
+  padding: 1rem;
+  top: -5.5%;
+  left: 0;
+  font-size: 2rem;
+  line-height: 0;
+  opacity: 1;
+  transition: all 300ms ease;
+  z-index: 4;
+}
+.detection:hover .card-info {
+  opacity: 0;
+}
 .card-links {
   position: absolute;
   padding: 1rem;
@@ -226,12 +242,10 @@ export default {
   transform-origin: left center 0;
   z-index: 4;
 }
-
 .detection:hover .card-links {
   transform: rotateY(0deg);
   opacity: 1;
 }
-
 .card-author {
   position: absolute;
   padding: 1rem;
@@ -240,29 +254,24 @@ export default {
   opacity: 0;
   transition: all 300ms ease;
   z-index: 3;
-
   display: flex;
-
   max-width: 116%;
   overflow: hidden;
+  pointer-events: none;
 }
-
 .detection:hover .card-author {
   opacity: 1;
 }
-
 .card-author span {
   background-color: rgba(0, 0, 0, 0.7);
   padding: 0.3rem 0.3rem 0.4rem;
   border: 1px solid #343a40;
   border-radius: 0.25rem;
   font-weight: bolder;
-
   white-space: nowrap;
   text-overflow: ellipsis;
   overflow: hidden;
 }
-
 .card-body {
   position: absolute;
   width: 143%;
@@ -273,31 +282,25 @@ export default {
   padding-top: 0;
   z-index: 3;
 }
-
 .detection:hover .card-body {
   opacity: 1;
 }
-
 .tweet-text {
   margin: 0;
 }
-
 .tweet-text >>> a {
   color: #80bdff;
   text-decoration: none;
   background-color: rgba(0, 0, 0, 0.7);
 }
-
 .tweet-text >>> a:hover {
   color: #3396ff;
 }
-
 .tweet-images {
   display: flex;
   position: relative;
   height: 100%;
 }
-
 .tweet-images::before {
   background-color: rgba(0, 0, 0, 0);
   position: absolute;
@@ -309,75 +312,61 @@ export default {
   transition: all 300ms ease;
   z-index: 1;
 }
-
 .detection:hover ~ .tweet-images::before {
   background-color: rgba(0, 0, 0, 0.4);
 }
-
 .tweet-image img {
   object-fit: cover;
   width: 100%;
   height: 100%;
 }
-
 .tweet-image {
   padding: 0;
 }
-
 /*2枚の場合*/
 .tweet-image.tweet-image-two-1 {
   border-right: 1px solid #10171e;
 }
-
 .tweet-image.tweet-image-two-2 {
   border-left: 1px solid #10171e;
 }
-
 /*3枚の場合*/
 .tweet-image.tweet-image-three-1 {
   border-right: 1px solid #10171e;
 }
-
 .tweet-image.tweet-image-three-outer {
   flex-wrap: wrap;
 }
-
 .tweet-image.tweet-image-three-2 {
   height: 50%;
   border-left: 1px solid #10171e;
   border-bottom: 1px solid #10171e;
 }
-
 .tweet-image.tweet-image-three-3 {
   height: 50%;
   border-left: 1px solid #10171e;
   border-top: 1px solid #10171e;
 }
-
 /*4枚の場合*/
 .tweet-image-four-outer {
   display: flex;
   flex-wrap: wrap;
 }
-
 .tweet-image.tweet-image-four-1 {
   height: 50%;
   border-right: 1px solid #10171e;
   border-bottom: 1px solid #10171e;
 }
-
 .tweet-image.tweet-image-four-2 {
   height: 50%;
   border-left: 1px solid #10171e;
   border-bottom: 1px solid #10171e;
 }
-
 .tweet-image.tweet-image-four-3 {
   height: 50%;
   border-right: 1px solid #10171e;
   border-top: 1px solid #10171e;
 }
-
 .tweet-image.tweet-image-four-4 {
   height: 50%;
   border-left: 1px solid #10171e;
---
+++ components/LightBox.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <CoolLightBox
+      :items="lightBoxData.mediaUrls"
+      :index="lightBoxData.index"
+      :loop="false"
+      :close-on-click-outside-mobile="true"
+      @close="lightBoxData.index = null"
+    >
+    </CoolLightBox>
+  </div>
+</template>
+
+<script>
+import CoolLightBox from 'vue-cool-lightbox'
+import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
+export default {
+  components: {
+    CoolLightBox,
+  },
+  data() {
+    return {
+      lightBoxData: {
+        mediaUrls: [],
+        index: null,
+      },
+    }
+  },
+  created() {
+    this.$nuxt.$on('LIGHT_BOX_EVENT', (lightBoxData) => {
+      this.lightBoxData = lightBoxData
+    })
+  },
+  beforeDestroy() {
+    this.$nuxt.$off('LIGHT_BOX_EVENT')
+  },
+}
+</script>
