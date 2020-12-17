73608952d1e99ba513b3c6031904604b1b1adf26
takashun <s1832087py@s.chibakoudai.jp>
2020-12-17 22:45:14 +0900

    スレッド機能削除

--- server/api/v1/api.js
+++ server/api/v1/api.js
@@ -34,7 +34,7 @@ router.get('/date/:date', function (req, res) {
     return res.status(500).send('parameter is missing!')
 
   let sql =
-    'SELECT SQL_CACHE id, cast(status_id as char) as status_id, cast(user_id as char) as user_id, user_name, tweet_text, media_urls, date, CASE WHEN EXISTS (SELECT id FROM tweets AS sub_tweets WHERE sub_tweets.status_id = tweets.parent_status_id) THEN 1 WHEN EXISTS (SELECT id FROM tweets AS sub_tweets WHERE sub_tweets.parent_status_id = tweets.status_id) THEN 1 ELSE 0 END AS thread_flag FROM `tweets` WHERE `date` = ? ORDER BY `id` DESC'
+    'SELECT SQL_CACHE id, cast(status_id as char) as status_id, cast(user_id as char) as user_id, user_name, tweet_text, media_urls, date FROM `tweets` WHERE `date` = ? ORDER BY `id` DESC'
   sql = mysql.format(sql, req.params.date)
 
   connection.query(sql, function (error, results, fields) {
@@ -53,7 +53,7 @@ router.get('/userid/:id', function (req, res) {
     return res.status(500).send('parameter is missing!')
 
   let sql =
-    'SELECT SQL_CACHE id, cast(status_id as char) as status_id, cast(user_id as char) as user_id, user_name, tweet_text, media_urls, MAX(date) AS date, CASE WHEN EXISTS (SELECT id FROM tweets AS sub_tweets WHERE sub_tweets.status_id = tweets.parent_status_id) THEN 1 WHEN EXISTS (SELECT id FROM tweets AS sub_tweets WHERE sub_tweets.parent_status_id = tweets.status_id) THEN 1 ELSE 0 END AS thread_flag FROM `tweets` WHERE `user_id` = ? GROUP BY status_id ORDER BY `id` DESC'
+    'SELECT SQL_CACHE id, cast(status_id as char) as status_id, cast(user_id as char) as user_id, user_name, tweet_text, media_urls, MAX(date) AS date FROM `tweets` WHERE `user_id` = ? GROUP BY status_id ORDER BY `id` DESC'
 
   sql = mysql.format(sql, req.params.id)
 
@@ -64,95 +64,12 @@ router.get('/userid/:id', function (req, res) {
   })
 })
 
-// // GET  http://localhost:3000/api/v1/thread/:id
-// router.get('/thread/:id', function (req, res) {
-//   res.header('Content-Type', 'application/json; charset=utf-8')
-//
-//   const regex = /^[1-9]\d*$/
-//   if (!regex.test(req.params.id))
-//     return res.status(500).send('parameter is missing!')
-//
-//   let sql =
-//     'WITH RECURSIVE cte1 (id, status_id, user_id, user_name, tweet_text, media_urls, date, parent_status_id) as ( SELECT id, status_id, user_id, user_name, tweet_text, media_urls, date, parent_status_id FROM tweets WHERE parent_status_id = ? UNION ALL SELECT temp.id, temp.status_id, temp.user_id, temp.user_name, temp.tweet_text, temp.media_urls, temp.date, temp.parent_status_id FROM tweets temp INNER JOIN cte1 ON temp.parent_status_id = cte1.status_id ), cte2 (id, status_id, user_id, user_name, tweet_text, media_urls, date, parent_status_id ) as ( SELECT id, status_id, user_id, user_name, tweet_text, media_urls, date, parent_status_id FROM tweets WHERE status_id = ? UNION ALL SELECT temp.id, temp.status_id, temp.user_id, temp.user_name, temp.tweet_text, temp.media_urls, temp.date, temp.parent_status_id FROM tweets temp INNER JOIN cte2 ON temp.status_id = cte2.parent_status_id ) SELECT id, cast(status_id as char) as status_id, cast(user_id as char) as user_id, user_name, tweet_text, media_urls, MAX(date) AS date FROM cte1 GROUP BY status_id UNION SELECT id, cast(status_id as char) as status_id, cast(user_id as char) as user_id, user_name, tweet_text, media_urls, MAX(date) AS date FROM cte2 GROUP BY status_id ORDER BY LPAD(status_id, 20, 0) ASC'
-//   sql = mysql.format(sql, [req.params.id, req.params.id])
-//
-//   connection.query(sql, function (error, results, fields) {
-//     if (error) return res.status(500).send('sql error!')
-//
-//     res.send({ results: makeResults(results) })
-//   })
-// })
-
-// GET  http://localhost:3000/api/v1/thread/:id
-router.get('/thread/:id', function (req, res) {
-  res.header('Content-Type', 'application/json; charset=utf-8')
-
-  const regex = /^[1-9]\d*$/
-  if (!regex.test(req.params.id))
-    return res.status(500).send('parameter is missing!')
-
-  let resultCte1
-  let resultCte2
-
-  const cte1 = new Promise(function (resolve, reject) {
-    let sql =
-      'WITH RECURSIVE\n' +
-      ' cte1 (id, status_id, user_id, user_name, tweet_text, media_urls, date, parent_status_id) as ( SELECT id, status_id, user_id, user_name, tweet_text, media_urls, date, parent_status_id\n' +
-      ' FROM tweets\n' +
-      ' WHERE parent_status_id = ?\n' +
-      ' UNION ALL SELECT temp.id, temp.status_id, temp.user_id, temp.user_name, temp.tweet_text, temp.media_urls, temp.date, temp.parent_status_id\n' +
-      ' FROM tweets temp\n' +
-      ' INNER JOIN cte1 ON temp.parent_status_id = cte1.status_id )\n' +
-      ' SELECT id, cast(status_id as char) as status_id, cast(user_id as char) as user_id, user_name, tweet_text, media_urls, MAX(date) AS date\n' +
-      ' FROM cte1\n' +
-      ' GROUP BY cte1.status_id'
-    sql = mysql.format(sql.replace(/\r?\n/g, ' '), [req.params.id])
-
-    connection.query(sql, function (error, results, fields) {
-      if (error) throw new Error('sql error!')
-
-      resultCte1 = results
-      resolve()
-    })
-  })
-
-  const cte2 = new Promise(function (resolve, reject) {
-    let sql =
-      'WITH RECURSIVE\n' +
-      'cte2 (id, status_id, user_id, user_name, tweet_text, media_urls, date, parent_status_id ) as ( SELECT id, status_id, user_id, user_name, tweet_text, media_urls, date, parent_status_id\n' +
-      ' FROM tweets\n' +
-      ' WHERE status_id = ?\n' +
-      ' UNION ALL SELECT temp.id, temp.status_id, temp.user_id, temp.user_name, temp.tweet_text, temp.media_urls, temp.date, temp.parent_status_id\n' +
-      ' FROM tweets temp\n' +
-      ' INNER JOIN cte2 ON temp.status_id = cte2.parent_status_id )\n' +
-      ' SELECT id, cast(status_id as char) as status_id, cast(user_id as char) as user_id, user_name, tweet_text, media_urls, MAX(date) AS date\n' +
-      ' FROM cte2\n' +
-      ' GROUP BY cte2.status_id'
-    sql = mysql.format(sql.replace(/\r?\n/g, ' '), [req.params.id])
-
-    connection.query(sql, function (error, results, fields) {
-      if (error) throw new Error('sql error!')
-
-      resultCte2 = results
-      resolve()
-    })
-  })
-
-  Promise.all([cte1, cte2])
-    .then(function () {
-      res.send({ results: makeResults(resultCte2.concat(resultCte1)) })
-    })
-    .catch(function (reason) {
-      res.status(500).send(reason)
-    })
-})
-
 // GET  http://localhost:3000/api/v1/random
 router.get('/random', function (req, res) {
   res.header('Content-Type', 'application/json; charset=utf-8')
 
   const sql =
-    'SELECT id, cast(status_id as char) as status_id, cast(user_id as char) as user_id, user_name, tweet_text, media_urls, date, CASE WHEN EXISTS (SELECT id FROM tweets AS sub_tweets WHERE sub_tweets.status_id = tweets.parent_status_id) THEN 1 WHEN EXISTS (SELECT id FROM tweets AS sub_tweets WHERE sub_tweets.parent_status_id = tweets.status_id) THEN 1 ELSE 0 END AS thread_flag FROM `tweets`WHERE RAND() <= (SELECT 100/COUNT(id) FROM tweets) ORDER BY RAND() LIMIT 36'
+    'SELECT id, cast(status_id as char) as status_id, cast(user_id as char) as user_id, user_name, tweet_text, media_urls, date FROM `tweets` WHERE RAND() <= (SELECT 100/COUNT(id) FROM tweets) ORDER BY RAND() LIMIT 36'
 
   connection.query(sql, function (error, results, fields) {
     if (error) return res.status(500).send('sql error!')
